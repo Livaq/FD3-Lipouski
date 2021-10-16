6ea04dfd9f0205eca0e645db1130de1850c14dec
Ivan Lipouski <livama8@gmail.com>
2021-10-16 10:08:41 +0300

    HW1 commit 1

---
+++ Homework-1-IShop/IShop.js
@@ -0,0 +1,33 @@
+﻿let IShop = React.createClass({
+
+    displayName: 'IShop',
+
+    render: function () {
+
+        let shopItemsData = [];
+        shopItems.forEach((item) => {
+            let itemDisplay =
+                React.DOM.div({key: item.code, className: 'IShop__shop-item_container'},
+                    React.DOM.span({className: 'IShop__shop-item_container_item'}, item.itemName),
+                    React.DOM.span({className: 'IShop__shop-item_container_item'}, item.itemPrice),
+                    React.DOM.span({className: 'IShop__shop-item_container_item'}, item.imgURL),
+                    React.DOM.span({className: 'IShop__shop-item_container_item'}, item.inStock),
+                );
+            shopItemsData.push(itemDisplay);
+        })
+
+
+        return React.DOM.div({className: 'IShop__categories_container'},
+            React.DOM.div({className: 'IShop__shop-name'}, this.props.shopName),
+            React.DOM.div({className: 'IShop__categories-list'},
+                React.DOM.span({className: 'IShop__categories_item'}, "Инструмент"),
+                React.DOM.span({className: 'IShop__categories_item'}, "Цена"),
+                React.DOM.span({className: 'IShop__categories_item'}, "Изображение"),
+                React.DOM.span({className: 'IShop__categories_item'}, "Количество В Наличии"),
+
+                ),
+            React.DOM.div({className: 'IShop__shop-name'}, shopItemsData),
+        );
+    },
+
+});
